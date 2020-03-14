eeb6971dbd1bbeecc7dd8c5f9d1d8d4a822c9290
harmitbadyal <harmitbadyal@gmail.com>
2020-03-14 13:03:31 -0400

    Working Drone Code

--- DroneApplication/app/src/main/java/com/example/droneapplication/MainActivity.java
+++ DroneApplication/app/src/main/java/com/example/droneapplication/MainActivity.java
@@ -1,6 +1,5 @@
 package com.example.droneapplication;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.localbroadcastmanager.content.LocalBroadcastManager;
 
@@ -10,6 +9,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.ServiceConnection;
+import android.net.sip.SipSession;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
@@ -20,16 +20,34 @@ import android.widget.ListView;
 import android.widget.TextView;
 
 import com.parrot.arsdk.ARSDK;
+import com.parrot.arsdk.arcommands.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DEVICE_STATE_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_DICTIONARY_KEY_ENUM;
+import com.parrot.arsdk.arcontroller.ARCONTROLLER_ERROR_ENUM;
+import com.parrot.arsdk.arcontroller.ARControllerArgumentDictionary;
+import com.parrot.arsdk.arcontroller.ARControllerCodec;
+import com.parrot.arsdk.arcontroller.ARControllerDictionary;
+import com.parrot.arsdk.arcontroller.ARControllerException;
+import com.parrot.arsdk.arcontroller.ARDeviceController;
+import com.parrot.arsdk.arcontroller.ARDeviceControllerListener;
+import com.parrot.arsdk.arcontroller.ARDeviceControllerStreamListener;
+import com.parrot.arsdk.arcontroller.ARFeatureARDrone3;
+import com.parrot.arsdk.arcontroller.ARFeatureCommon;
+import com.parrot.arsdk.arcontroller.ARFrame;
+import com.parrot.arsdk.ardiscovery.ARDISCOVERY_PRODUCT_ENUM;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryDevice;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceNetService;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryDeviceService;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryException;
+import com.parrot.arsdk.ardiscovery.ARDiscoveryMux;
 import com.parrot.arsdk.ardiscovery.ARDiscoveryService;
 import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiver;
 import com.parrot.arsdk.ardiscovery.receivers.ARDiscoveryServicesDevicesListUpdatedReceiverDelegate;
+import com.parrot.arsdk.arsal.ARSALPrint;
 
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity implements ARDiscoveryServicesDevicesListUpdatedReceiverDelegate {
+public class MainActivity extends AppCompatActivity implements ARDiscoveryServicesDevicesListUpdatedReceiverDelegate, ARDeviceControllerStreamListener {
 
     private static final String TAG = "DroneDiscoverer";
 
@@ -43,17 +61,67 @@ public class MainActivity extends AppCompatActivity implements ARDiscoveryServic
 
     private ServiceConnection mArdiscoveryServiceConnection;
 
+    private ARDiscoveryDevice mARDiscoveryDevice;
+
+    private ARDeviceController mARDeviceController;
+
+    private ARCONTROLLER_DEVICE_STATE_ENUM mState;
+
     TextView viewer;
 
-    Button findNetworks;
+    Button findNetworks,connectDrone,takeOffBtn,landBtn;
 
     ListView wifiViewer;
 
+    private final ARDeviceControllerListener mDeviceControllerListener = new ARDeviceControllerListener() {
+        @Override
+        public void onStateChanged(ARDeviceController deviceController, ARCONTROLLER_DEVICE_STATE_ENUM newState, ARCONTROLLER_ERROR_ENUM error) {
+            switch (newState) {
+                case ARCONTROLLER_DEVICE_STATE_RUNNING:
+                    break;
+                case ARCONTROLLER_DEVICE_STATE_STOPPED:
+                    break;
+                case ARCONTROLLER_DEVICE_STATE_STARTING:
+                    break;
+                case ARCONTROLLER_DEVICE_STATE_STOPPING:
+                    break;
 
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void onExtensionStateChanged(ARDeviceController deviceController, ARCONTROLLER_DEVICE_STATE_ENUM newState, ARDISCOVERY_PRODUCT_ENUM product, String name, ARCONTROLLER_ERROR_ENUM error) {
+
+        }
+
+        @Override
+        public void onCommandReceived(ARDeviceController deviceController, ARCONTROLLER_DICTIONARY_KEY_ENUM commandKey, ARControllerDictionary elementDictionary) {
+            if (elementDictionary != null) {
+                // if the command received is a battery state changed
+                if (commandKey == ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED) {
+                    ARControllerArgumentDictionary<Object> args = elementDictionary.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+
+                    if (args != null) {
+                        Integer batValue = (Integer) args.get(ARFeatureCommon.ARCONTROLLER_DICTIONARY_KEY_COMMON_COMMONSTATE_BATTERYSTATECHANGED_PERCENT);
+                        Integer flyingStateInt = (Integer) args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE);
+                        //ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.getFromValue(flyingStateInt);
+                        // do what you want with the battery level
+                    }
+                }
+            } else {
+                Log.e(TAG, "elementDictionary is null");
+            }
+        }
+    };
 
     public MainActivity() {
         this.mArdiscoveryServicesDevicesListUpdatedReceiver = null;
         this.mContext = null;
+        mARDiscoveryDevice=null;
+        myDeviceService = null;
+        mState = ARCONTROLLER_DEVICE_STATE_ENUM.ARCONTROLLER_DEVICE_STATE_STOPPED;
     }
 
     @Override
@@ -61,8 +129,11 @@ public class MainActivity extends AppCompatActivity implements ARDiscoveryServic
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         findNetworks = findViewById(R.id.findNetworks);
+        connectDrone = findViewById(R.id.connectBtn);
         wifiViewer = findViewById(R.id.wifiViewer);
         viewer = findViewById(R.id.viewer);
+        takeOffBtn = findViewById(R.id.takeOffBtn);
+        landBtn = findViewById(R.id.landBtn);
         initDiscoveryService();
         ARSDK.loadSDKLibs();
         initDiscoveryService();
@@ -80,15 +151,15 @@ public class MainActivity extends AppCompatActivity implements ARDiscoveryServic
 
             String [] deviceListString = new String[deviceList.size()];
             int x=0;
-                for(ARDiscoveryDeviceService device1:deviceList)
-                {
-                    deviceListString[x]=device1.toString();
-                    x++;
-                }
+            for(ARDiscoveryDeviceService device1:deviceList)
+            {
+                deviceListString[x]=device1.toString();
+                x++;
+            }
             ArrayAdapter <String> adapter = new ArrayAdapter(this,layout.simple_list_item_1,deviceListString);
             wifiViewer.setAdapter(adapter);
-            viewer.setText(deviceList.size()+"");
             myDeviceService = deviceList.get(0);
+            viewer.setText(deviceList.get(0).toString());
             // Do what you want with the device list
         }
     }
@@ -101,6 +172,52 @@ public class MainActivity extends AppCompatActivity implements ARDiscoveryServic
                 onServicesDevicesListUpdated();
             }
         });
+        connectDrone.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                createDeviceController();
+            }
+        });
+        takeOffBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                takeoff();
+            }
+        });
+        landBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                land();
+            }
+        });
+    }
+
+    public void createDeviceController(){
+        mARDiscoveryDevice = createDiscoveryDevice(myDeviceService);
+        try {
+            mARDeviceController = new ARDeviceController(mARDiscoveryDevice);
+        }
+        catch(ARControllerException e)
+        {
+            e.printStackTrace();
+        }
+        unregisterReceivers();
+        closeServices();
+        viewer.setText(mARDeviceController.toString());
+        mARDeviceController.addListener((mDeviceControllerListener));
+        if ((mARDeviceController != null) && (ARCONTROLLER_DEVICE_STATE_ENUM.ARCONTROLLER_DEVICE_STATE_STOPPED.equals(mState))) {
+            ARCONTROLLER_ERROR_ENUM error = mARDeviceController.start();
+            if(error!=ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK)
+                finish();
+        }
+        viewer.setText("Connected");
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mARDeviceController.dispose();
+        viewer.setText("Disoposed");
     }
 
     private void initDiscoveryService()
@@ -147,6 +264,7 @@ public class MainActivity extends AppCompatActivity implements ARDiscoveryServic
         }
     }
 
+
     private void registerReceivers()
     {
         mArdiscoveryServicesDevicesListUpdatedReceiver = new ARDiscoveryServicesDevicesListUpdatedReceiver((ARDiscoveryServicesDevicesListUpdatedReceiverDelegate) this);
@@ -154,14 +272,55 @@ public class MainActivity extends AppCompatActivity implements ARDiscoveryServic
         localBroadcastMgr.registerReceiver(mArdiscoveryServicesDevicesListUpdatedReceiver, new IntentFilter(ARDiscoveryService.kARDiscoveryServiceNotificationServicesDevicesListUpdated));
     }
 
-    private ARDiscoveryDevice createDiscoveryDevice(@NonNull ARDiscoveryDeviceService service) {
-        ARDiscoveryDevice device = null;
-        try {
-            device = new ARDiscoveryDevice(mContext, service);
-        } catch (ARDiscoveryException e) {
-            Log.e(TAG, "Exception", e);
-        }
+    private void takeoff()
+    {
+        if (ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_LANDED.equals(getPilotingState()))
+        {
+            ARCONTROLLER_ERROR_ENUM error = mARDeviceController.getFeatureARDrone3().sendPilotingTakeOff();
 
+            if (!error.equals(ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK))
+            {
+                ARSALPrint.e(TAG, "Error while sending take off: " + error);
+            }
+        }
+    }
+
+    private void land()
+    {
+        ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = getPilotingState();
+        if (ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING.equals(flyingState) ||
+                ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_HOVERING.equals(flyingState))
+        {
+            ARCONTROLLER_ERROR_ENUM error = mARDeviceController.getFeatureARDrone3().sendPilotingLanding();
+
+            if (!error.equals(ARCONTROLLER_ERROR_ENUM.ARCONTROLLER_OK))
+            {
+                ARSALPrint.e(TAG, "Error while sending take off: " + error);
+            }
+        }
+    }
+
+    private ARDiscoveryDevice createDiscoveryDevice(ARDiscoveryDeviceService service)
+    {
+        ARDiscoveryDevice device = null;
+        if ((service != null)&&
+                (ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_BEBOP_2.equals(ARDiscoveryService.getProductFromProductID(service.getProductID()))))
+        {
+            viewer.setText("reached here");
+            try
+            {
+                device = new ARDiscoveryDevice();
+
+                ARDiscoveryDeviceNetService netDeviceService = (ARDiscoveryDeviceNetService) service.getDevice();
+
+                device.initWifi(ARDISCOVERY_PRODUCT_ENUM.ARDISCOVERY_PRODUCT_ARDRONE, netDeviceService.getName(), netDeviceService.getIp(), netDeviceService.getPort());
+            }
+            catch (ARDiscoveryException e)
+            {
+                e.printStackTrace();
+                Log.e(TAG, "Error: " + e.getError());
+            }
+        }
         return device;
     }
 
@@ -190,4 +349,51 @@ public class MainActivity extends AppCompatActivity implements ARDiscoveryServic
             }).start();
         }
     }
+
+    private ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM getPilotingState()
+    {
+        ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.eARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_UNKNOWN_ENUM_VALUE;
+        if (mARDeviceController != null)
+        {
+            try
+            {
+                ARControllerDictionary dict = mARDeviceController.getCommandElements(ARCONTROLLER_DICTIONARY_KEY_ENUM.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED);
+                if (dict != null)
+                {
+                    ARControllerArgumentDictionary<Object> args = dict.get(ARControllerDictionary.ARCONTROLLER_DICTIONARY_SINGLE_KEY);
+                    if (args != null)
+                    {
+                        Integer flyingStateInt = (Integer) args.get(ARFeatureARDrone3.ARCONTROLLER_DICTIONARY_KEY_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE);
+                        flyingState = ARCOMMANDS_ARDRONE3_PILOTINGSTATE_FLYINGSTATECHANGED_STATE_ENUM.getFromValue(flyingStateInt);
+                    }
+                }
+            }
+            catch (ARControllerException e)
+            {
+                e.printStackTrace();
+            }
+
+            return flyingState;
+        }
+        return null;
+    }
+
+
+
+    @Override
+    public ARCONTROLLER_ERROR_ENUM configureDecoder(ARDeviceController deviceController, ARControllerCodec codec) {
+        return null;
+    }
+
+    @Override
+    public ARCONTROLLER_ERROR_ENUM onFrameReceived(ARDeviceController deviceController, ARFrame frame) {
+        return null;
+    }
+
+    @Override
+    public void onFrameTimeout(ARDeviceController deviceController) {
+
+    }
+
+
 }
