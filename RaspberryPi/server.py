2511cdd3c5c2b165d42717a7e451822e27d4cf66
Harmit Badyal <harmitbadyal@gmail.com>
2020-03-15 17:23:14 -0400

    RaspberryPi Files

--- RaspberryPi/server.py
+++ RaspberryPi/server.py
@@ -5,9 +5,14 @@
 # $Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
 
 from bluetooth import *
+import os
+import sys
 
+
+writeFile = open('outputData/writeFile.txt','w+')
+writeFile.close();
 server_sock=BluetoothSocket( RFCOMM )
-server_sock.bind(("",PORT_ANY))
+server_sock.bind(("", PORT_ANY))
 server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
@@ -27,16 +32,30 @@ print("Waiting for connection on RFCOMM channel %d" % port)
 client_sock, client_info = server_sock.accept()
 print("Accepted connection from ", client_info)
 
-try:
-    while True:
-        data = client_sock.recv(1024)
-        if len(data) == 0: break
-        print("received [%s]" % data)
-except IOError:
-    pass
+newPid = os.fork()
+
+if newPid==0:
+    try:
+        while True:
+            data = client_sock.recv(1024)
+            if len(data) == 0: break
+            print("Server: %s" % data)
+            writeFile = open('outputData/writeFile.txt','a') 
+            writeFile.write("%s\n" % data)
+            writeFile.close();
+    except IOError:
+        pass
+else:
+    try:
+        while True:
+            sendData = raw_input()
+            if not sendData:
+                break;
+            client_sock.send(sendData)
+    except IOError:
+        pass
 
 print("disconnected")
-
 client_sock.close()
 server_sock.close()
 print("all done")
